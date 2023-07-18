3f265932f507206cf97811587b2ae0cac3a77af1
ratul <ratul@M1-MacBook-Air.local>
2023-07-19 02:15:19 +0600

    fix card

--- src/modules/user/role/NewRole.tsx
+++ src/modules/user/role/NewRole.tsx
@@ -33,7 +33,7 @@ const NewRole = () => {
         <Card
           title="New Role"
           style={{
-            width: "80%",
+            width: "90%",
             backgroundColor: "#ffffff",
             borderRadius: "10px",
             margin: "0 auto",
