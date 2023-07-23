91074e89db8712bde12a441456c7106ea81ef3b7
anikkhann46 <anikkhann46@gmail.com>
2023-07-23 13:12:47 +0600

    package design update

--- src/modules/package/package/NewPackage.tsx
+++ src/modules/package/package/NewPackage.tsx
@@ -31,13 +31,16 @@ const NewPackage = () => {
         />
 
         <Card
-          title="New package"
+          title="New Package"
+          hoverable
           style={{
             width: "90%",
             backgroundColor: "#ffffff",
             borderRadius: "10px",
             margin: "0 auto",
-            textAlign: "center"
+            textAlign: "center",
+            marginTop: "3rem",
+            marginBottom: "3rem"
           }}
         >
           <CreatePackageForm />
