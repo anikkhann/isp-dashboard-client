b24b944a0039d1e791db911a1e440413fa379524
ratul <ratul@M1-MacBook-Air.local>
2023-08-12 20:06:02 +0600

    update dashboard

---
+++ src/components/dashboard/client/CustomerCardData.tsx
@@ -0,0 +1,202 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import AppRowContainer from "@/lib/AppRowContainer";
+import TableCard from "@/lib/TableCard";
+import { useQuery } from "@tanstack/react-query";
+import { Card, Col, Space, Table } from "antd";
+import axios from "axios";
+import Cookies from "js-cookie";
+import React, { useEffect, useState } from "react";
+import { AlignType } from "rc-table/lib/interface";
+import type { ColumnsType } from "antd/es/table";
+
+const CustomerCardData = () => {
+  const [data, setData] = useState<any[]>([]);
+
+  const fetchData = async () => {
+    const token = Cookies.get("token");
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+    const { data } = await axios.get(
+      `/api/dashboard/get-total-active-customer`,
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+    return data;
+  };
+
+  const { isLoading, isError, error, isFetching } = useQuery<boolean, any>({
+    queryKey: ["dashboard-active-customer-list"],
+    queryFn: async () => {
+      const response = await fetchData();
+      return response;
+    },
+    onSuccess(data: any) {
+      if (data) {
+        // console.log("data.data", data);
+
+        if (data.body) {
+          setData(data.body);
+        } else {
+          setData([]);
+        }
+      }
+    },
+    onError(error: any) {
+      console.log("error", error);
+    }
+  });
+
+  useEffect(() => {
+    if (data) {
+      setData(data);
+    }
+  }, [data]);
+
+  const columns: ColumnsType<any> = [
+    {
+      title: "Serial",
+      dataIndex: "id",
+      render: (tableParams, row, index) => {
+        return (
+          <>
+            <Space>{index + 1}</Space>
+          </>
+        );
+      },
+      sorter: false,
+      width: "10%",
+      align: "center" as AlignType
+    },
+    // client
+    {
+      title: "Client",
+      dataIndex: "client",
+      sorter: false,
+      render: (client: any) => {
+        if (!client) return "-";
+        return <>{client}</>;
+      },
+      /* width: "20%", */
+      align: "center" as AlignType
+    },
+    // total_customer
+    {
+      title: "Total Customer",
+      dataIndex: "total_customer",
+      sorter: false,
+      render: (total_customer: any) => {
+        if (!total_customer) return "-";
+        return <>{total_customer}</>;
+      },
+      /* width: "20%", */
+      align: "center" as AlignType
+    },
+    // active_customer
+    {
+      title: "Active Customer",
+      dataIndex: "active_customer",
+      sorter: false,
+      render: (active_customer: any) => {
+        if (!active_customer) return "-";
+        return <>{active_customer}</>;
+      },
+      /* width: "20%", */
+      align: "center" as AlignType
+    },
+    // registered_customer
+    {
+      title: "Registered Customer",
+      dataIndex: "registered_customer",
+      sorter: false,
+      render: (registered_customer: any) => {
+        if (!registered_customer) return "-";
+        return <>{registered_customer}</>;
+      },
+      /* width: "20%", */
+      align: "center" as AlignType
+    },
+    // expired_customer
+    {
+      title: "Expired Customer",
+      dataIndex: "expired_customer",
+      sorter: false,
+      render: (expired_customer: any) => {
+        if (!expired_customer) return "-";
+        return <>{expired_customer}</>;
+      },
+      /* width: "20%", */
+      align: "center" as AlignType
+    }
+  ];
+
+  return (
+    <>
+      <>
+        <AppRowContainer>
+          <Col span={24} key="data-f">
+            {isError && (
+              <>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    margin: " 10px 5px"
+                  }}
+                >
+                  <Card
+                    title="Error"
+                    style={{
+                      width: 300,
+                      color: "#FF5630",
+                      backgroundColor: "#ffffff"
+                    }}
+                  >
+                    <p>
+                      {error &&
+                      error.response &&
+                      error.response.data &&
+                      error.response.data.message
+                        ? error.response.data.message
+                        : error.message
+                        ? error.message
+                        : "Something went wrong"}
+                    </p>
+                  </Card>
+                </div>
+              </>
+            )}
+
+            <TableCard
+              title="Active Customer List"
+              hasLink={false}
+              addLink=""
+              permission=""
+              style={{
+                borderRadius: "10px",
+                padding: "10px",
+                width: "100%",
+                overflowX: "auto",
+                backgroundColor: "#ffffff"
+              }}
+            >
+              <Space direction="vertical" style={{ width: "100%" }}>
+                <Table
+                  columns={columns}
+                  rowKey={record => record.client}
+                  dataSource={data}
+                  loading={isLoading || isFetching}
+                />
+              </Space>
+            </TableCard>
+          </Col>
+        </AppRowContainer>
+      </>
+    </>
+  );
+};
+
+export default CustomerCardData;
--- src/modules/dashboard/ClientDashboard.tsx
+++ src/modules/dashboard/ClientDashboard.tsx
@@ -4,92 +4,149 @@ import ClientYearlyColumnChart from "@/components/charts/clientDashboard/ClientY
 import CustomerMonthlyColumnChart from "@/components/charts/clientDashboard/CustomerMonthlyColumnChart";
 import CustomerQuarterColumnChart from "@/components/charts/clientDashboard/CustomerQuarterColumnChart";
 import CustomerYearlyColumnChart from "@/components/charts/clientDashboard/CustomerYearlyColumnChart";
+import ClientCardData from "@/components/dashboard/client/ClientCardData";
+import CustomerCardData from "@/components/dashboard/client/CustomerCardData";
 import AppAnimate from "@/lib/AppAnimate";
 import AppRowContainer from "@/lib/AppRowContainer";
-import { Card, Col } from "antd";
-import React from "react";
+import { Card, Col, Select, Space } from "antd";
+import React, { useState } from "react";
+
+const clientDataType = [
+  {
+    label: "Monthly New Clients",
+    value: "monthly"
+  },
+
+  {
+    label: "Quarterly New Clients",
+    value: "quarterly"
+  },
+  {
+    label: "Yearly New Clients",
+    value: "yearly"
+  }
+];
+
+const customerDataType = [
+  {
+    label: "Monthly New Customers",
+    value: "monthly"
+  },
+  {
+    label: "Quarterly New Customers",
+    value: "quarterly"
+  },
+  {
+    label: "Yearly New Customers",
+    value: "yearly"
+  }
+];
 
 const ClientDashboard = () => {
+  const [selectedClientDataType, setSelectedClientDataType] = useState<string>(
+    clientDataType[0].value
+  );
+
+  const [selectedCustomerDataType, setSelectedCustomerDataType] =
+    useState<string>(customerDataType[0].value);
+
+  const handleChange = (value: string) => {
+    setSelectedClientDataType(value);
+  };
+
+  const handleChangeCustomer = (value: string) => {
+    setSelectedCustomerDataType(value);
+  };
+
   return (
     <>
       <AppAnimate>
         <AppRowContainer>
+          <Col span={24}>
+            <ClientCardData />
+          </Col>
+
           <Col xs={24} lg={12}>
             <Card
-              title="Monthly New Clients"
+              title={
+                <>
+                  {selectedClientDataType === "monthly"
+                    ? "Monthly New Clients"
+                    : selectedClientDataType === "yearly"
+                    ? "Yearly New Clients"
+                    : "Quarterly New Clients"}
+                </>
+              }
               style={{
                 width: "100%",
                 backgroundColor: "#fff",
                 borderRadius: "10px"
               }}
+              extra={
+                <Space style={{ width: "100%" }} direction="vertical">
+                  <Select
+                    allowClear
+                    style={{ width: "100%", textAlign: "start" }}
+                    placeholder="Please select"
+                    onChange={handleChange}
+                    options={clientDataType}
+                    value={selectedClientDataType}
+                  />
+                </Space>
+              }
             >
-              <ClientMonthlyColumnChart />
+              {selectedClientDataType === "monthly" ? (
+                <ClientMonthlyColumnChart />
+              ) : selectedClientDataType === "yearly" ? (
+                <ClientYearlyColumnChart />
+              ) : (
+                <ClientQuarterColumnChart />
+              )}
             </Card>
           </Col>
 
           <Col xs={24} lg={12}>
             <Card
-              title="Yearly New Clients"
+              title={
+                <>
+                  {selectedCustomerDataType === "monthly"
+                    ? "Monthly New Customers"
+                    : selectedCustomerDataType === "yearly"
+                    ? "Yearly New Customers"
+                    : "Quarterly New Customers"}
+                </>
+              }
               style={{
                 width: "100%",
                 backgroundColor: "#fff",
                 borderRadius: "10px"
               }}
+              extra={
+                <Space style={{ width: "100%" }} direction="vertical">
+                  <Select
+                    allowClear
+                    style={{ width: "100%", textAlign: "start" }}
+                    placeholder="Please select"
+                    onChange={handleChangeCustomer}
+                    options={customerDataType}
+                    value={selectedCustomerDataType}
+                  />
+                </Space>
+              }
             >
-              <ClientYearlyColumnChart />
+              {selectedCustomerDataType === "monthly" ? (
+                <CustomerMonthlyColumnChart />
+              ) : selectedCustomerDataType === "yearly" ? (
+                <CustomerYearlyColumnChart />
+              ) : (
+                <CustomerQuarterColumnChart />
+              )}
             </Card>
           </Col>
-
-          <Col xs={24} lg={12}>
-            <Card
-              title="Quarter New Clients"
-              style={{
-                width: "100%",
-                backgroundColor: "#fff",
-                borderRadius: "10px"
-              }}
-            >
-              <ClientQuarterColumnChart />
-            </Card>
-          </Col>
-
-          <Col xs={24} lg={12}>
-            <Card
-              title="Monthly New Customers"
-              style={{
-                width: "100%",
-                backgroundColor: "#fff",
-                borderRadius: "10px"
-              }}
-            >
-              <CustomerMonthlyColumnChart />
-            </Card>
-          </Col>
-
-          <Col xs={24} lg={12}>
-            <Card
-              title="Yearly New Customers"
-              style={{
-                width: "100%",
-                backgroundColor: "#fff",
-                borderRadius: "10px"
-              }}
-            >
-              <CustomerYearlyColumnChart />
-            </Card>
-          </Col>
-
-          <Col xs={24} lg={12}>
-            <Card
-              title="Quarter New Customers"
-              style={{
-                width: "100%",
-                backgroundColor: "#fff",
-                borderRadius: "10px"
-              }}
-            >
-              <CustomerQuarterColumnChart />
-            </Card>
+        </AppRowContainer>
+        <AppRowContainer>
+          <Col span={24}>
+            <CustomerCardData />
           </Col>
         </AppRowContainer>
       </AppAnimate>
