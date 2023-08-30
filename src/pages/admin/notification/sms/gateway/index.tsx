abf27b162ebd7f86b922bb39570f48d1f8dcfe4d
ratul <ratul@M1-MacBook-Air.local>
2023-08-30 13:08:28 +0600

    update menu

--- src/pages/admin/notification/sms/gateway/index.tsx
+++ src/pages/admin/notification/sms/gateway/index.tsx
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <>
       {auth.isLoading && <AppLoader />}
-      {ability.can("smsGateway.view", "") ? <GatewayList /> : <Forbidden />}
+      {ability.can("smsGateway.list", "") ? <GatewayList /> : <Forbidden />}
     </>
   );
 };
