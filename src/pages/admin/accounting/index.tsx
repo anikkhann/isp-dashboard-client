6cc00d5ef735308d11e5c01238de5d003917e36d
shahriar_ratul <ratul794@gmail.com>
2023-09-24 15:49:00 +0600

    fix permission

--- src/pages/admin/accounting/index.tsx
+++ src/pages/admin/accounting/index.tsx
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <>
       {auth.isLoading && <AppLoader />}
-      {ability.can("accounting.zoneTransaction", "") ? (
+      {ability.can("accounting.dashboard", "") ? (
         <AccountDashboard />
       ) : (
         <Forbidden />
